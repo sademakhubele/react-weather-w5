ff5c779f425cb2bd6a4c55eefddabc068f67ca28
sademakhubele <157542678+sademakhubele@users.noreply.github.com>
2024-07-17 15:36:48 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
+import React from 'react';
+import Weather from './Weather';
 import './App.css';
-import "bootstrap/dist/css/bootstrap.css";
 
-function App() {
-  return (
-    <div className='App'>
-      <div className='container'>
-
-      </div>
+export default function App() {
+  return(
+  <div className='App'>
+    <div className='container'>
+    <h1>
+      Weather Forecast
+    </h1>
+      <Weather cityName={"Lisbon"}/>
+      <footer>
+      Coded by Saba and it is{" "}
+       <a href='https://github.com/SabaKD/react-weather-application'>Open-sourced on github</a>
+        
+       </footer>
     </div>
+  </div>
   );
 }
-
-export default App;
--- src/Weather.js
+++ src/Weather.js
@@ -1,15 +1,14 @@
-import React , {useState} from "react";
+import React , { useState } from "react";
 import axios from "axios";
 import "./Weather.css";
 import WeatherInfo from "./WeatherInfo";
 import WeatherForecast from "./WeatherForecast";
 
-export default function Weather(props) {
+export default function Weather(props){
     const [weatherData, setWeatherData] = useState({ready : false});
     const [forecastWeatherData, setForecastWeatherData] = useState({ready : false});
-    const [city, setCity] = useState(props.cityName);
-
-    function handleResponse (response) {
+    const [city, setCity] = useState(props.cityName);   
+    function handleResponse (response){
         setWeatherData({
             ready: true,
             newCity: city,
@@ -17,24 +16,32 @@ export default function Weather(props) {
             wind: response.data.wind.speed,
             humidity: response.data.temperature.humidity,
             description: response.data.condition.description,
-            feelsLike: response.data.temperature.feelsLike,
-            icon: response.data.icon_url,
+            feelsLike: response.data.temperature.feels_like,
+            icon: response.data.condition.icon_url,
             date: new Date(response.data.time *1000)
 
         })
+        
+    }
+    function handleForecast(response){
+        setForecastWeatherData({
+            ready: true,
+            forecast: response.data.daily
+        })
+
     }
 
-    function handleSubmit(event) {
+    
+    function handleSubmit(event){
         event.preventDefault();
         search();
     }
-
-    function handleCityChange() {
+    function handleCityChange(event){
         setCity(event.target.value);
+        
     }
-
-    function search() {
-        let apiKey = "c6f8ef4575250284954db9f4dfa7a996"
+    function search(){
+        let apiKey = "4bc76te01aac743d12o764377f46072e";
         let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
         let forecastApiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
 
@@ -42,8 +49,8 @@ export default function Weather(props) {
         axios.get(forecastApiUrl).then(handleForecast);
 
     }
-
-    if (weatherData.ready) {
+    
+    if (weatherData.ready){
         return (
             <div className="Weather">
             <form onSubmit={handleSubmit}>
@@ -61,27 +68,29 @@ export default function Weather(props) {
             
 
         </div>
-        )
-    }
-
-    return (
-
-        <div className="Weather">
+        
+    );
+    } else {
+        search();
+        return ( 
+            
+            
+            <div className="Weather">
             <form onSubmit={handleSubmit}>
                 <div className="row">
                     <div className="col-9">
-                        <input type="search" placeholder="Enter city" className="form-search" onChange={handleCityChange} autoFocus="on"></input>
+                        <input type="search" placeholder="Enter a city.." className="form-control" autoFocus="on" onChange={handleCityChange}/>
                     </div>
                     <div className="col-3">
-                        <input type="submit" value={search} className="btn btn-danger w-100"></input>
+                        <input type="submit" value="Search" className="btn btn-primary w-100"/>
                     </div>
                 </div>
             </form>
             <div>
-                <strong>Loadind...</strong>
+                <strong>Loading...</strong>
             </div>
-            <div>
             </div>
-        </div>
-    )
+        );
+    }
+    
 }
